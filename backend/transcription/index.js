9efa5ebe32acd78f018963df455bab9e156216d2
wlren <weilinwork99@gmail.com>
2023-01-14 15:49:53 +0800

    uwu

--- backend/transcription/index.js
+++ backend/transcription/index.js
@@ -1,5 +1,15 @@
+var ytdl = require("ytdl-core");
+const fs = require("fs");
+
 const processLink = async (link) => {
-  console.log("Hello");
+    const video = ytdl(link, { filter: "audioonly", quality: "highestaudio" });
+    console.log(video);
+
+    var newLink = link.split("v=")[1];
+
+    video.pipe(fs.createWriteStream("temp/audio/" + newLink + ".mp4"));
 };
+
+processLink("https://www.youtube.com/watch?v=9bZkp7q19f0");
 
 module.exports = { processLink };
