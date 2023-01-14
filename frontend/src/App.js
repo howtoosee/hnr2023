ee9ab94edcb4991d3efea86508bde633a6687695
Xiong Jingya <xiongjingyaa@gmail.com>
2023-01-14 15:53:23 +0800

    Add some frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+import TextInput from './textinput';
+import WelcomeHeader from './header';
+import { useState } from 'react';
+
 function App() {
+  const [link, setLink] = useState("");
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <WelcomeHeader/>
+        <TextInput />
       </header>
     </div>
   );
---
+++ frontend/src/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './header.css';
+
+const WelcomeHeader = () => {
+    
+    return (
+        <div>
+            <p>
+                Welcome to <b>The Summarizer</b>!
+            </p>
+            <p className='Header'>
+                Paste the link of your video in the text box below to get a short summary of your lecture.
+            </p>
+        </div>
+    );
+}
+
+export default WelcomeHeader;
