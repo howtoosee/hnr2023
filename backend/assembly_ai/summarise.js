61713877b2d6dcf8688a4675d6f8da76dfe39d6a
Chen Xihao <chenxihao0627@gmail.com>
2023-01-14 15:15:43 +0800

    Add AI api calls

---
+++ backend/assembly_ai/summarise.js
@@ -0,0 +1,34 @@
+import { upload_file } from "./upload";
+const axios = require("axios");
+
+
+const assembly = axios.create({
+    baseURL: "https://api.assemblyai.com/v2",
+    headers: {
+        authorization: process.env.ASSEMBLY_AI_KEY,
+    },
+});
+
+const assembly_summarise = function(file) {
+    const url = upload_file(file);
+
+    let summarised_text = "";
+
+    assembly.post("/transcript", {
+            audio_url: url,
+            summarization: true,
+            summary_model: "informative",
+            summary_type: "bullets_verbose"
+        })
+        .then((res) => {summarised_text = res.data})
+        .catch((err) => {
+            console.error(err);
+            summarised_text = "ML processing error."
+        });
+    
+    return summarised_text;
+}
+
+module.exports = {
+    assembly_summarise,
+}
