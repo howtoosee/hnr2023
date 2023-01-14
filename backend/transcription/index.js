9223917e7c2df87dd1403544a28740b90f1072fb
wlren <weilinwork99@gmail.com>
2023-01-14 16:19:37 +0800

    Buffer

--- backend/transcription/index.js
+++ backend/transcription/index.js
@@ -3,11 +3,15 @@ const fs = require("fs");
 
 const processLink = async (link) => {
     const video = ytdl(link, { filter: "audioonly", quality: "highestaudio" });
-    console.log(video);
 
     var newLink = link.split("v=")[1];
 
     video.pipe(fs.createWriteStream("temp/audio/" + newLink + ".mp4"));
+
+    var buffer = fs.readFile("temp/audio/" + newLink + ".mp4", (err, data) => {
+        if (err) throw err;
+        console.log(data);
+    });
 };
 
 processLink("https://www.youtube.com/watch?v=9bZkp7q19f0");
